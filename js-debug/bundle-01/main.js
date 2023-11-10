83b88696b183961638e406c8433c1a1349310f7e
GianmarcoDandrea <gianm96@hotmail.it>
2023-11-10 16:05:44 +0100

    add: bundle 1

--- js-debug/bundle-01/main.js
+++ js-debug/bundle-01/main.js
@@ -11,37 +11,51 @@
 
 
 // ESERCIZIO 1
-for (let i = 0; i > 5; i++) {
+for (let i = 0; i < 5; i++) { // rimosso il maggiore e messo il minore perchè la variabile parte da 0  
     console.log(i);
 }
 
 
 // ESERCIZIO 2
+
+console.log(addIfEven(6));
+
 function addIfEven(num) {
-    if (num % 2 = 0) {
+    if (num % 2 === 0) { // aggiunto il simbolo di uguaglianza invece di 
         return num + 5;
     }
-    return num;
+;
 }
 
 
+
 // ESERCIZIO 3
+
+console.log(loopToFive());
 function loopToFive() {
-    for (let i = 0, i < 5, i++) {
+    for (let i = 0; i < 6; i++) {// rimosso , e messo ; tolto il 5 messo il 6 per far arrivare il ciclo a 5
         console.log(i);
     }
 }
 
 
 // ESERCIZIO 4 (suggerimento: ci sono 7 errori)
+
+displayEvenNumbers()
+console.log(displayEvenNumbers()); // dovrebbe restituire [2,4,6,8]
+
 function displayEvenNumbers() {
-    let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+    
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8]; //rimosso let e aggiunto const
     let evenNumbers = [];
-    for (let i = 0; i < numbers.length - 1; i++;) {
-        if (numbers % 2 = 0); {
-            evenNumbers.push(i);
+    for (let i = 0; i < numbers.length; i++) { //rimosso ; finale e tolto - 1 alla condizione centrale
+
+        if ((numbers[i] % 2) === 0); { // aggiunto simbolo ugualianza e [i] dopo il numbers
+
+            evenNumbers.push(numbers[i]); //aggiunto numbers[i]
+
         }
-        return evenNumbers;
     }
+    return evenNumbers; //rimosso da dentro l'if 
 }
-displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
+
